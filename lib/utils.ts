ac70bcbd2ed0ef32fa1ae4c4ef96307d9e9a8363
ZhangLihua <54661719+Lien-hua@users.noreply.github.com>
2025-06-30 14:20:10 +0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 // lib/utils.ts
 import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-variants';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: any[]) {
   return twMerge(clsx(inputs));
